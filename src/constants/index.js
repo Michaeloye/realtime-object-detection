366d6281530fa1c562c0802a515722fead8b3cec
michaeloye <michaeloye23@gmail.com>
2024-06-22 17:23:07 +0100

    Change internet speed limit

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export const MIC_UPPER_LIMIT = 10;
 export const MIC_LOWER_LIMIT = 1;
-export const INTERNET_SPEED_UPPER_LIMIT = 25;
-export const INTERNET_SPEED_LOWER_LIMIT = 10;
+export const INTERNET_SPEED_UPPER_LIMIT = 10;
+export const INTERNET_SPEED_LOWER_LIMIT = 3;
 export const LIGHTING_UPPER_LIMIT = 0.75;
 export const LIGHTING_LOWER_LIMIT = 0.5;
